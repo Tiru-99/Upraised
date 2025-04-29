c6c961b15eb7c30c8936d3f7d614cc5ce095c998
Tiru-99 <aayushtirmanwar1234@gmail.com>
2025-04-29 22:03:53 +0530

    redis port number changed

--- server/src/utils/redis.ts
+++ server/src/utils/redis.ts
@@ -10,7 +10,7 @@ const client = createClient({
     password: process.env.REDIS_PASSWORD,
     socket: {
         host: process.env.REDIS_HOST,
-        port: 19453
+        port: 19903
     }
 });
 
